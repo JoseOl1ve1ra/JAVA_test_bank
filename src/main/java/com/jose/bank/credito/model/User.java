415e97c59d86222a2db6408b15d572d67d3c0e2e
Jose Oliveira <jose.oliveira.adm@gmail.com>
2023-03-15 18:49:37 -0300

    primeiro commit

---
+++ src/main/java/com/jose/bank/credito/model/User.java
@@ -0,0 +1,13 @@
+package com.jose.bank.credito.model;
+
+import jakarta.persistence.Table;
+
+@Table(name= "tb_user", schema = "message_generator")
+public class User {
+
+    //private string name;
+    //private string lastName;
+    //private string login;
+    //private stringer age;
+
+}
---
+++ src/test/java/com/jose/bank/credito/MessageGeneratorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jose.bank.credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Olá Mundo");
+	}
+
+}
